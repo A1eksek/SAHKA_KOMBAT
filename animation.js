b77510f4f8098d307a4dc938c16f4ec4364da007
A1eksek <Alexey20080705@gmail.com>
2024-08-27 20:21:12 +0400

    Add files via upload

--- animation.js
+++ animation.js
@@ -7,3 +7,13 @@ document.getElementById('Home_butt').addEventListener('click', function(){
             this.style.transform = 'scale(1) translateY(0)';
         }, 100); 
 })
+
+document.getElementById('hideButton').addEventListener('click', function() {
+    var myDiv = document.getElementById('upgrade');
+    myDiv.classList.add('hidden');
+})
+
+document.getElementById('showButton').addEventListener('click', function() {
+    var myDiv = document.getElementById('upgrade'); // Получаем элемент div
+    myDiv.classList.remove('hidden'); // Убираем класс moved, чтобы вернуть элемент обратно
+});
